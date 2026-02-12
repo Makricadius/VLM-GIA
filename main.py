8cb1085d05e91ca7b768ba65b847053b971b30d3
Makricadius <tomas.herranzpastora@gmail.com>
2026-02-12 13:55:17 +0100

    Simple test of a wing

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from geometry import *
+from singularities import *
+from aerodynamics import *
 
-
+geometric_wing = trapezoidal_simetrical_wing(superficie=100, alargamiento=6, estrechamiento=0, torsión=-3, flecha=35, diedro=0)
+ALA = Aerdynamic_wing(geometric_wing, Vortex_shoe)
+ALA.generate_model()
+ALA.solve(alpha=5)
+print(ALA.alpha_memory[5]["A"])

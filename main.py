4e42d4d504ee7552124247b68bb47f0c2f699cd4
Makricadius <tomas.herranzpastora@gmail.com>
2026-02-13 16:16:29 +0100

    Delta-wing test

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from geometry import *
 from singularities import *
 from aerodynamics import *
-from post_processing import plot_aero_characteristics
+from post_processing import *
 
-geometric_wing = trapezoidal_simetrical_wing(superficie=200, alargamiento=11, estrechamiento=1, torsión=0, flecha=5, diedro=0)
+geometric_wing = trapezoidal_simetrical_wing(superficie=150, alargamiento=3, estrechamiento=0.1, torsión=-7, flecha=40, diedro=10)
 ALA = Aerdynamic_wing(geometric_wing, Vortex_shoe)
-ALA.generate_model(31)
+ALA.generate_model(32)
 
 density = 10
 [ALA.calculate(alpha=e/density) for e in range(-5*density,5*density+1)]
 
 geometric_wing.plot_nodes()
 plot_aero_characteristics(ALA, show=True)
+
+plot_streamlines_3d(ALA, alpha=7.0, stream_box_scale=(1.5, 1.2, 0.5), smoke_scale=(1.1, 0.2), 
+                    stream_density=(11, 5), grid_resolution=(30, 23, 10), 
+                    upstream_offset=0, max_length=1, step_size=0.04, show=True)
